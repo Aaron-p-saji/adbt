8decafd7994cde3b2a2d34e0ed8b1d788503ca48
Aaron-p-saji <65056958+Aaron-p-saji@users.noreply.github.com>
2022-07-24 12:22:05 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -44,7 +44,7 @@ async def delete(user, message):
        else:
           await asyncio.sleep(TIME)
           await Bot.delete_messages(message.chat.id, message.message_id)
-        await message.reply('Hai There This Is Test')
+          await message.reply(START_MSG)
     except Exception as e:
        print(e)
        
